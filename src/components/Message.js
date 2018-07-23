1b71defbb93ad09c1397ac20411a0c4712b16a5d
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-07-23 12:24:20 -0700

    added more info indicator and made cursor pointer

--- src/components/Message.js
+++ src/components/Message.js
@@ -5,8 +5,14 @@ import './Chatroom.css';
 
 class Message extends React.Component {
 
-  updateMoreInfo(msgData) {
-    if ('extra_data' in msgData && 'answer_data' in msgData.extra_data && msgData.extra_data.answer_data !== '') {
+  hasMoreInfo() {
+    const msgData = this.props.messageData;
+    return ('extra_data' in msgData && 'answer_data' in msgData.extra_data && msgData.extra_data.answer_data !== '')
+  }
+
+  updateMoreInfo() {
+    const msgData = this.props.messageData;
+    if (this.hasMoreInfo()) {
       const data = {
         'detail': msgData.extra_data.answer_data.detail,
         'source': msgData.extra_data.answer_data.source
@@ -26,12 +32,12 @@ class Message extends React.Component {
       user = <img src={BotImg} alt='bot' />
     }
     return (
-      <li className={`chat ${this.props.user === "bot" ? "left" : "right"}`}
-        onClick={(e) => this.updateMoreInfo(this.props.messageData)}>
+      <li className={`chat ${this.props.user === "bot" ? "left" : "right"} ${this.hasMoreInfo() ? "hasMoreInfo" : ""}`}
+        onClick={(e) => this.updateMoreInfo()}>
         {/* {user !== chat.username
         && <img src={chat.img} alt={`${chat.username}'s profile pic`} />
       } */}
-      {user} {text}
+      {user} {text}{this.hasMoreInfo() && '...'}
     </li>);
   }
 }
