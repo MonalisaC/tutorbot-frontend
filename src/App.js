3d8b125fb1f3ebd5aa272c587501daf6a13e8d6d
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-07-24 22:55:50 -0700

    background color changes, added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import tutorcuriebotImg from './tutorcuriebot.jpg';
+import sitemaintainerImg from './site-maintainer.jpg';
+import logotwitterImg from './logo-twitter.png';
+import logolinkedinImg from './logo-linkedin.png';
+import githubImg from './github-mark.png';
+
 
 import { Chatroom } from './containers/Chatroom'
 import { MoreInformation } from './containers/MoreInformation'
@@ -20,6 +25,20 @@ class App extends Component {
           <Chatroom />
           <MoreInformation />
         </div>
+        <footer className="Footer">
+          <div><img className="bot" src={tutorcuriebotImg} alt="TutorCurieBot"/> TutorCurieBot © 2018</div>
+          <div className="maintainedby">
+            <div>Site maintained by MonalisaC</div>
+            <div className="hidden">
+              <img className="rounded-corners" src={sitemaintainerImg} alt="myself"/>
+            </div>
+          </div>
+          <div>
+            <a href="https://twitter.com/madamecurie10"><img className="profile" src={logotwitterImg} alt="Twitter"/></a>
+            <a href="https://www.linkedin.com/in/monalisa-chatterjee"><img className="profile" src={logolinkedinImg} alt="Linkedin"/></a>
+            <a href="https://github.com/MonalisaC"><img className="profile" src={githubImg} alt="Github"/></a>
+          </div>
+        </footer>
       </div>
     );
   }
